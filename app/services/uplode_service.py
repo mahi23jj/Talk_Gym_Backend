7107fe53b4a3f2be09ab5634c89c120e726af753
mahlet <mahlet2ambaye@gmail.com>
2026-04-07 17:26:18 +0100

    fix: remove comment

--- app/services/uplode_service.py
+++ app/services/uplode_service.py
@@ -6,11 +6,6 @@ def upload_audio_to_cloudinary(file_path: str ) -> str:
     if file_path and file_path.endswith((".mp3", ".wav", ".ogg")):
         file_size = os.path.getsize(file_path)
 
-        # if attempt_type == "speed" and file_size > 5 * 1024 * 1024:  # 5MB for speed attempts
-        #     raise ValueError("File size exceeds the 5MB limit for speed attempts")
-        # elif attempt_type == "concise" and file_size > 2 * 1024 * 1024:  # 2MB for concise attempts
-        #     raise ValueError("File size exceeds the 2MB limit for concise attempts")
-       
     else:
         raise ValueError("Invalid file type. Only .mp3, .wav, and .ogg are allowed.")
 
